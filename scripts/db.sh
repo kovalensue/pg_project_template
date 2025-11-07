c24c510d28a2ae083a6b1d5fec95a744a4dde701
kovalensue <tomas.kovalik@gmail.com>
2025-11-07 09:11:44 +0100

    Refactor database readiness check in db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -4,7 +4,10 @@ echo "Starting local database(s)..."
 docker compose -f ./docker/docker-compose.yml down -t 0 -v
 docker compose -f ./docker/docker-compose.yml up -d
 echo -n "Waiting for database(s) "
-until [ "$(docker ps | grep -vc "healthy" || echo 0)" -eq 1 ]; do sleep 1; echo -n "."; done
+while docker compose -f ./docker/docker-compose.yml ps --format "{{.Status}}" | grep -vq "healthy"; do
+  sleep 1
+  echo -n "."
+done
 echo -n " Database(s) ready!"
 echo
 ./scripts/orchestrate_plan.py
